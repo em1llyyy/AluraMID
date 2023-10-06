777a8f700c1217decc775a4624df7fcea9f4c611
em1llyyy <106530972+em1llyyy@users.noreply.github.com>
2023-10-06 10:56:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-document.querySelector('.tecla_pom');
-function tocaSomPom() {
-document.querySelector('#som_tecla_pom').play();
+alert('Olá, mundo')
+function ticaSom(iElementoAudio){
+    document.querySelector(iElementoAudio).play()
+}
+document.querySelectorAll('.tecla');
+listaDeTecla[0].onclick = tocaSom;
+const listaDeTecla = document.querySelectorAll('.tecla');
+listaDeTecla[0].onclick = tocaSomPom;
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador < listaDeTecla.length) {
+    listaDeTecla[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log[contadaor];
 }
