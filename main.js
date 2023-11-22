5d5a7cb4d0c20bf88cabc45a2ea71c5306e7fb64
em1llyyy <106530972+em1llyyy@users.noreply.github.com>
2023-11-22 09:46:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,77 @@
+// Pom -------------------------------------------
+function tocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+/*
+     Clap
+*/
+function tocaSomClap (){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+/*
+     Tim
+*/
+function tocaSomTim (){
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+/*
+     Puff
+*/
+function tocaSomPuff (){
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+/*
+     Splash
+*/
+function tocaSomSplash (){
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+/*
+    Toim
+*/
+function tocaSomToim (){
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+/*
+     Psh
+*/
+function tocaSomPsh (){
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+/*
+     Tic
+*/
+function tocaSomTic (){
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+/*
+     Tom
+*/
+function tocaSomTom (){
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+document.querySelectorAll('.tecla');
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
 alert('Olá, mundo')
 function tocaSom(iElementoAudio){
 const elementom = document.querySelector(iElementoAudio).play()
@@ -9,8 +83,6 @@ if (elemento.localName === 'audio') {
     alert('Elemento não encontrado');
 }
 }
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
 for(let contador = 0; contador < listaDeTeclas.length; contador++) {
